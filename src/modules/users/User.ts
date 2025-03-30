05a5eb7fb5fd1df6d6665edcef4d243b0641d583
Elor-Itz <elor2200@gmail.com>
2025-03-30 14:16:49 +0300

    Refractored code

---
+++ src/modules/users/User.ts
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+  _id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  profilePicture?: string;
+  headline?: string;
+  bio?: string
+  location?: string;
+  website?: string;
+  joinDate?: string;  
+  refreshToken?: string[];
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+  headline: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  joinDate: {
+    type: String,
+  },
+  refreshToken: {
+    type: [String],
+    default: [],
+  }
+});
+
+const userModel = mongoose.model<IUser>("Users", userSchema);
+
+export default userModel;
