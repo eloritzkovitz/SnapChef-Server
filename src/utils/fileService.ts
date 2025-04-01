e7dd46c3d9d125280d5e51c6dbf4ea1f25eff246
Elor-Itz <elor2200@gmail.com>
2025-04-01 11:45:40 +0300

    Fixed image pathing

--- src/utils/fileService.ts
+++ src/utils/fileService.ts
@@ -6,15 +6,10 @@ import path from 'path';
  * @param filePath - The path to the file to be deleted.
  */
 export const deleteFile = (filePath: string): void => {
-    const resolvedPath = resolveFilePath(filePath);
-    if (fs.existsSync(resolvedPath)) fs.unlinkSync(resolvedPath);  
-};
-
-/**
- * Resolves the full path to a file based on the provided relative path.
- * @param relativePath - The relative path to the file.
- * @returns The full path to the file.
- */
-export const resolveFilePath = (relativePath: string): string => {
-    return relativePath ? path.join(__dirname, '../../', relativePath.replace(`${process.env.BASE_URL}`, '/dist')) : '';          
+    if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        console.log(`File deleted: ${filePath}`);
+    } else {
+        console.log(`File not found: ${filePath}`);
+    }
 };
