6e2637ffcff23f64af0c938049e4a1876c229315
Elor-Itz <elor2200@gmail.com>
2025-04-17 16:36:02 +0300

    Updated cookbook routes and controllers

--- src/modules/cookbook/cookbookRoutes.ts
+++ src/modules/cookbook/cookbookRoutes.ts
@@ -1,12 +1,8 @@
 import express from "express";
-import {
-  saveRecipe,
-  removeSavedRecipe,
-  shareRecipe,
-  getSavedRecipes,
-} from "./cookbookController";
+import cookbookController from "./cookbookController";
 
 const router = express.Router();
+
 /**
  * @swagger
  * tags:
@@ -16,92 +12,152 @@ const router = express.Router();
 
 /**
  * @swagger
- * /api/cookbook/save:
+ * /api/cookbook/{cookbookId}/recipes:
  *   post:
- *     summary: Save a recipe to the cookbook
- *     tags: [Cookbook]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               userId:
- *                 type: string
- *               recipeId:
- *                 type: string
- *     responses:
- *       201:
- *         description: Recipe saved
- */
-router.post("/save", saveRecipe);
-
-/**
- * @swagger
- * /api/cookbook/remove:
- *   delete:
- *     summary: Remove a recipe from the cookbook
- *     tags: [Cookbook]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               userId:
- *                 type: string
- *               recipeId:
- *                 type: string
- *     responses:
- *       200:
- *         description: Recipe removed
- */
-
-router.delete("/remove", removeSavedRecipe);
-/**
- * @swagger
- * /api/cookbook/share:
- *   post:
- *     summary: Share a saved recipe with a friend
- *     tags: [Cookbook]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               userId:
- *                 type: string
- *               recipeId:
- *                 type: string
- *               friendId:
- *                 type: string
- *     responses:
- *       200:
- *         description: Recipe shared successfully
- */
-router.post("/share", shareRecipe);
-
-/**
- * @swagger
- * /api/cookbook:
- *   get:
- *     summary: Get all saved recipes for a user
+ *     summary: Add a recipe to the cookbook
  *     tags: [Cookbook]
  *     parameters:
- *       - in: query
- *         name: userId
+ *       - in: path
+ *         name: cookbookId
  *         required: true
  *         schema:
  *           type: string
+ *         description: The ID of the cookbook
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               mealType:
+ *                 type: string
+ *               cuisineType:
+ *                 type: string
+ *               difficulty:
+ *                 type: string
+ *               cookingTime:
+ *                 type: number
+ *               prepTime:
+ *                 type: number
+ *               ingredients:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               instructions:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               imageURL:
+ *                 type: string
  *     responses:
  *       200:
- *         description: List of saved recipes
+ *         description: Recipe added to the cookbook
  */
+router.post("/:cookbookId/recipes", cookbookController.addRecipe);
 
-router.get("/", getSavedRecipes);
+/**
+ * @swagger
+ * /api/cookbook/{cookbookId}/recipes/{recipeId}:
+ *   put:
+ *     summary: Update a recipe in the cookbook
+ *     tags: [Cookbook]
+ *     parameters:
+ *       - in: path
+ *         name: cookbookId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the cookbook
+ *       - in: path
+ *         name: recipeId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the recipe
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               mealType:
+ *                 type: string
+ *               cuisineType:
+ *                 type: string
+ *               difficulty:
+ *                 type: string
+ *               cookingTime:
+ *                 type: number
+ *               prepTime:
+ *                 type: number
+ *               ingredients:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               instructions:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               imageURL:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Recipe updated in the cookbook
+ */
+router.put("/:cookbookId/recipes/:recipeId", cookbookController.updateRecipe);
+
+/**
+ * @swagger
+ * /api/cookbook/{cookbookId}/recipes/{recipeId}:
+ *   delete:
+ *     summary: Remove a recipe from the cookbook
+ *     tags: [Cookbook]
+ *     parameters:
+ *       - in: path
+ *         name: cookbookId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the cookbook
+ *       - in: path
+ *         name: recipeId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the recipe
+ *     responses:
+ *       200:
+ *         description: Recipe removed from the cookbook
+ */
+router.delete("/:cookbookId/recipes/:recipeId", cookbookController.removeRecipe);
+
+/**
+ * @swagger
+ * /api/cookbook/{cookbookId}:
+ *   get:
+ *     summary: Get a cookbook with all recipes
+ *     tags: [Cookbook]
+ *     parameters:
+ *       - in: path
+ *         name: cookbookId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the cookbook
+ *     responses:
+ *       200:
+ *         description: The cookbook with all recipes
+ */
+router.get("/:cookbookId", cookbookController.getCookbookContent);
 
 export default router;
