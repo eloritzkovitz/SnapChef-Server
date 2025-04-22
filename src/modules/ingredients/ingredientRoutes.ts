f2e695575b74be7b2163555868c8574345135869
Elor-Itz <elor2200@gmail.com>
2025-04-22 09:40:17 +0300

    Added missing documentation

--- src/modules/ingredients/ingredientRoutes.ts
+++ src/modules/ingredients/ingredientRoutes.ts
@@ -32,8 +32,7 @@ const router = express.Router();
  *         description: Recognized ingredients from photo
  */
 // Define routes
-router.post("/recognize/photo", upload.single("file"), (req, res) =>  ingredientController.recognize(req, res, "photo")
-);
+router.post("/recognize/photo", upload.single("file"), (req, res) =>  ingredientController.recognize(req, res, "photo"));
 
 /**
  * @swagger
@@ -79,9 +78,35 @@ router.post("/recognize/receipt", upload.single("file"), (req, res) => ingredien
  *       200:
  *         description: Recognized ingredients from barcode
  */
-router.post("/recognize/barcode", upload.single("file"), (req, res) => ingredientController.recognize(req, res, "barcode")
-);
+router.post("/recognize/barcode", upload.single("file"), (req, res) => ingredientController.recognize(req, res, "barcode"));
 
+/**
+ * @swagger
+ * /api/ingredients:
+ *   get:
+ *     summary: Get all ingredients or search by query
+ *     tags: [Ingredients]
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Search ingredients by name
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         description: Search ingredients by category
+ *     responses:
+ *       200:
+ *         description: A list of ingredients
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Ingredient'
+ */
 router.get("/", (req, res) => {
   // Check if query parameters are provided
   if (Object.keys(req.query).length > 0) {
@@ -91,6 +116,29 @@ router.get("/", (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/ingredients/{id}:
+ *   get:
+ *     summary: Get an ingredient by ID
+ *     tags: [Ingredients]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the ingredient
+ *     responses:
+ *       200:
+ *         description: The ingredient details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Ingredient'
+ *       404:
+ *         description: Ingredient not found
+ */
 router.get("/:id", (req, res) => ingredientController.getIngredientById(req, res));
 
 export default router;
