3dbbd97c63505828de7d1626a1bbaa85e9a581c8
Elor Itzkovitz <96355233+Elor-Itz@users.noreply.github.com>
2025-05-05 20:13:50 +0300

    Revert "Add logging to user, recipe, ingredient, and cookbook modules"

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,6 @@ import recipesRoute from "./modules/recipes/recipeRoutes";
 import fridgeRoutes from "./modules/fridge/fridgeRoutes";
 import cookbookRoutes from "./modules/cookbook/cookbookRoutes";
 import notificationsRoutes from "./modules/notifications/notificationRoutes";
-import logRoutes from "./modules/logs/logRoutes";
-
-
 
 const app = express();
 
@@ -49,7 +46,6 @@ app.use("/api/fridge", fridgeRoutes);
 app.use("/api/recipes", recipesRoute);
 app.use("/api/cookbook",cookbookRoutes);
 app.use("/api/notifications", notificationsRoutes);
-app.use("/api/logs", logRoutes);
 
 app.get("/about", (req, res) => {
   res.send("This is the API for the SnapChef application.");
