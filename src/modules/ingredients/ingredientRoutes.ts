a17e94fb05e62231ee727b8f73cf51d0bc7a50f2
Elor-Itz <elor2200@gmail.com>
2025-05-22 17:17:41 +0300

    Decoupled recognition endpoints from ingredients

--- src/modules/ingredients/ingredientRoutes.ts
+++ src/modules/ingredients/ingredientRoutes.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import ingredientController from "./ingredientController";
-import upload from "../../middlewares/upload";
 import { authenticate, requireAdmin } from "../../middlewares/auth";
 
 const router = express.Router();
@@ -8,78 +7,8 @@ const router = express.Router();
  * @swagger
  * tags:
  *   name: Ingredients
- *   description: API for recognizing ingredients from images, receipts or barcodes
+ *   description: API for fetching and managing ingredients
  */
-
-/**
- * @swagger
- * /api/ingredients/recognize/photo:
- *   post:
- *     summary: Recognize ingredients from a photo
- *     tags: [Ingredients]
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               file:
- *                 type: string
- *                 format: binary
- *                 description: Image file to analyze
- *     responses:
- *       200:
- *         description: Recognized ingredients from photo
- */
-// Define routes
-router.post("/recognize/photo", authenticate, upload.single("file"), (req, res) =>  ingredientController.recognize(req, res, "photo"));
-
-/**
- * @swagger
- * /api/ingredients/recognize/receipt:
- *   post:
- *     summary: Recognize ingredients from a receipt image
- *     tags: [Ingredients]
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               file:
- *                 type: string
- *                 format: binary
- *                 description: Image file of a receipt
- *     responses:
- *       200:
- *         description: Recognized ingredients from receipt
- */
-router.post("/recognize/receipt", authenticate, upload.single("file"), (req, res) => ingredientController.recognize(req, res, "receipt"));
-
-/**
- * @swagger
- * /api/ingredients/recognize/barcode:
- *   post:
- *     summary: Recognize ingredients from a barcode image
- *     tags: [Ingredients]
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               file:
- *                 type: string
- *                 format: binary
- *                 description: Image file of a barcode
- *     responses:
- *       200:
- *         description: Recognized ingredients from barcode
- */
-router.post("/recognize/barcode", authenticate, (req, res) => ingredientController.recognize(req, res, "barcode"));
 
 /**
  * @swagger
