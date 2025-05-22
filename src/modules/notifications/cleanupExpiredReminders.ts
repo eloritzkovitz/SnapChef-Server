1b1bc1c970727751635bf8f202c237862aa92077
Elor-Itz <elor2200@gmail.com>
2025-05-23 00:08:27 +0300

    Added cron job for deleting expired reminders

---
+++ src/modules/notifications/cleanupExpiredReminders.ts
@@ -0,0 +1,11 @@
+import Notification from '../../modules/notifications/Notification';
+import logger from '../../utils/logger';
+
+export const deleteExpiredReminders = async () => {
+  const now = new Date();
+  const result = await Notification.deleteMany({
+    type: { $in: ['expiry', 'grocery'] },
+    scheduledTime: { $lt: now }
+  });
+  logger.info('Deleted %d expired reminders', result.deletedCount);
+};
