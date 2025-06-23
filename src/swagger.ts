daecbf025e524df43cdd01d91952fbff59f27997
Elor-Itz <elor2200@gmail.com>
2025-06-23 21:07:34 +0300

    Updated version

--- src/swagger.ts
+++ src/swagger.ts
@@ -9,7 +9,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "SnapChef API",
-      version: "1.0.0",
+      version: "1.0.1",
       description: "API server for the SnapChef application",
     },
     tags: [
