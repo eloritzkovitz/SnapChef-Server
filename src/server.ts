52cf0a11f14d05828a00d29fe13584e8f5421539
Elor-Itz <elor2200@gmail.com>
2025-07-01 20:51:02 +0300

    Small fix

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ const app = express();
 dotenv.config();
 
 // Ensure the uploads directory exists
-const uploadDir = path.join(__dirname, "../../uploads");
+const uploadDir = path.resolve(__dirname, '../../uploads');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
